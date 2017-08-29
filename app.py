7aceabfe3f13dcd152c201943bb510f351c97620
Emeka Icha <emeka.icha@gmail.com>
2017-08-29 11:35:54 +0300

    (feat): add user registration route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, session
+from flask import Flask, session, flash
 from flask import request
 from flask import abort,redirect, url_for
 from flask import render_template
@@ -14,12 +14,24 @@ from models import User, Post, Tag
 
 @app.route('/')
 def index():
+  if 'username' in session:
+    return render_template('index.html', username=username)
   return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
   if request.method == 'POST':
-    session['username'] = request.form['username']
+    username = request.form['username']
+    email = request.form['email']
+    if not username or not email:
+      flash('username or email is wrong')
+      return render_template('login.html')
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+      flash('user does not exist. Register to continue')
+      return render_template('login.html')
+
+    session['username'] = username 
     return redirect(url_for('index'))
   return render_template('login.html')
 
@@ -30,7 +42,28 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-  pass 
+  errors = {}
+  if request.method == 'POST':
+    username = request.form['username']
+    email = request.form['email']
+    username_exists = User.query.filter_by(username=username).first()
+    email_exists = User.query.filter_by(email=email).first()
+    if username_exists not None:
+      errors[username_error] = 'username already exists'
+    if email_exists not None:
+      errors[email_error] = 'email already exists'
+    if errors:
+      return render_template('register.html', errors=errors)
+    user = User(username=username, email=email)
+    session.add(user)
+    session.commit()
+    flash('successfully registered. Login to continue')
+    return redirect(url_for('login'))
+  return render_template('register.html')
+
+    
+
+  
 
 
 app.secret_key = '\x16Y\xe7\x97\x0e\x84n\ntH\xa3\x107I3kE\xa0\xc8\xaayFYz'
